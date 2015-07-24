5f27e0eeac85052cd87e462edb1cac16d431a039
Roger Fernandez Guri <rfguri@gmail.com>
2015-07-25 00:01:20 +0200

    Add methods to setup EUSART and send byte

--- P2F1/src/org/p2f1/controllers/MainWindowController.java
+++ P2F1/src/org/p2f1/controllers/MainWindowController.java
@@ -15,11 +15,14 @@ import java.nio.charset.Charset;
 
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
+import javax.swing.JProgressBar;
 
 import org.p2f1.models.MainWindowModel;
 import org.p2f1.views.MainWindowView;
 
 import com.SerialPort.SerialPort;
+
+// TODO File path /Users/Roger/Documents/eclipse/P2F1/src/org/p2f1/assets/data.txt
 
 public class MainWindowController implements ActionListener{
 
@@ -46,13 +49,18 @@ public class MainWindowController implements ActionListener{
 			JButton btn = (JButton) e.getSource();
 			switch(btn.getName()){
 				case MainWindowView.BTN_INFRAROJOS:
-					//TODO Afegir el codi per enviar per IR
+					//TODO TX/RX IR
 					JOptionPane.showMessageDialog(null, "M'has de programar!", "Missatge",JOptionPane.INFORMATION_MESSAGE);
 					break;
 				case MainWindowView.BTN_UART:
-					//TODO Afegir el codi per enviar per UART
+					//TODO TX/RX protocol 
 					if(model.checkInputFile(view.getFile())){
-						JOptionPane.showMessageDialog(null, "M'has de programar!", "Missatge",JOptionPane.INFORMATION_MESSAGE);
+						try{
+							setupUART();
+							sendBytes();
+						}catch (Exception e1){
+							e1.printStackTrace();
+						}
 					}else{
 						JOptionPane.showMessageDialog(null, "Has de seleccionar un fitxer de text!", "Missatge",JOptionPane.ERROR_MESSAGE);
 					}
@@ -64,5 +72,45 @@ public class MainWindowController implements ActionListener{
 	public void showView(){
 		view.setVisible(true);
 	}
+	
+	private void setupUART() throws Exception{
+		String sPort = view.getPort();
+		int baudrate = view.getBaudrate();
+		System.out.printf("Opening port %s @%d..\n", sPort, baudrate);
+		sp.openPort(sPort, baudrate);
+	}
+	
+	private void sendBytes() throws Exception{
+		File file = new File(view.getFile());
+		int size = (int)file.length();
+		InputStream in = new FileInputStream(file);
+		Reader reader = new InputStreamReader(in, Charset.defaultCharset()); 
+		Reader buffer = new BufferedReader(reader);
+		Thread t = new Thread(new Runnable(){
+	        public void run(){
+	        	try{
+					handleCharacters(buffer, size);
+				}catch (Exception e) {
+					e.printStackTrace();
+				}
+	        }
+	    });
+	    t.start();
+		
+	}
+	
+	private void handleCharacters(Reader reader, int size) throws Exception{
+        int b, i = 0, step = 100/size;
+        i += step;
+        while ((b = reader.read()) != -1){
+        	Thread.sleep(100);
+        	i += step;
+        	view.updateProgressBar(i);
+        	sp.writeByte((byte) b);
+        	System.out.printf("rcv: %s\n",(char) sp.readByte());
+        }
+        sp.closePort();
+        view.updateProgressBar(100);
+    }
 	
 }
