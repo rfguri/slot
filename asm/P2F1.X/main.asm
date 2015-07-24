ae3cbec4779709b25f5a1e7876296212337fb3f5
Roger Fernandez Guri <rfguri@gmail.com>
2015-07-25 00:12:51 +0200

    Add echo server

--- asm/P2F1.X/main.asm
+++ asm/P2F1.X/main.asm
@@ -16,9 +16,15 @@
 ;* CONFIGURATIONS *
 ;******************
 
+    CONFIG OSC = INTIO1
+    CONFIG PBADEN = DIG
+    CONFIG WDT = OFF
+
 ;*************
 ;* VARIABLES *
 ;*************
+
+TEMP EQU 0x00
  
 ;*************
 ;* CONSTANTS *
@@ -52,20 +58,52 @@ LOW_INT
 ;*********
 
 INIT_CPU
+    MOVLW 0x74
+    MOVWF OSCCON ; 8MHz Internal Oscilator
+    BSF OSCTUNE, PLLEN, 0
+    
     RETURN
     
 INIT_PORTS
+    CLRF TRISB
+    CLRF PORTB
     RETURN
 
 INIT_INTERRUPTS
+    BSF RCON, IPEN, 0
+    BCF INTCON, GIE/GIEH, 0
+    BCF INTCON, PEIE/GIEL, 0
     RETURN
 
 INIT_TXRX
+    MOVLW 0x00
+    MOVWF SPBRGH
+    MOVLW 0x19
+    MOVWF SPBRG
+    BCF TXSTA, BRGH, 0
+    BCF BAUDCON, BRG16, 0 ; Baud rate generator at 19200
+    BCF TXSTA, SYNC, 0 ; Asynchronous mode
+    BSF RCSTA, SPEN, 0 ; Enable serial
+    BSF TXSTA, TXEN, 0 ; Enable transmision
+    BSF RCSTA, CREN, 0 ; Enable reception
     RETURN
 
 ;*************
 ;* FUNCTIONS *
 ;*************
+
+SEND
+    BSF TXSTA, TXEN, 0
+    MOVWF TXREG, 0
+WAITTX
+    BTFSS TXSTA, TRMT
+    GOTO WAITTX
+    RETURN
+SERIAL
+    MOVFF RCREG, TEMP
+    MOVF TEMP, 0
+    CALL SEND
+    RETURN
 
 ;********
 ;* MAIN *
@@ -78,6 +116,9 @@ MAIN
     CALL INIT_TXRX
 
 LOOP
+    BTFSC PIR1, RCIF
+    CALL SERIAL
+    
     GOTO LOOP
 
 ;*******
